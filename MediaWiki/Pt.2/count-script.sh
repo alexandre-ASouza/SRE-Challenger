f492feb13af9d0da287d8226345842f5301d170f
alexandre-ASouza <39465493+alexandre-ASouza@users.noreply.github.com>
2020-08-23 19:20:24 -0300

    Update count-script.sh

--- MediaWiki/Pt.2/count-script.sh
+++ MediaWiki/Pt.2/count-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -s "http://mediawiki-challenger-1182272360.sa-east-1.elb.amazonaws.com/api.php?action=query&list=allpages&format=json" -o cont-pages.txt
+curl -s "http://mediawiki-challenger-1182272360.sa-east-1.elb.amazonaws.com/api.php?action=query&list=allpages&format=json" -o count-pages.txt
 echo " "
-tr "},{" "\n" < cont-pages.txt |sed 's/"title"/"Título da Página"/g' |grep "Título*"
-tr "},{" "\n" < cont-pages.txt |grep title|wc -l
+tr "},{" "\n" < count-pages.txt |sed 's/"title"/"Título da Página"/g' |grep "Título*"
+tr "},{" "\n" < count-pages.txt |grep title|wc -l
